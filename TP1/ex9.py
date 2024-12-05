79fdd0ac2217a33178bc7a4253f07e008e45f902
fatimazahra889 <fatimazahraema.88@gmail.com>
2024-12-05 12:32:41 +0100

    Modify ex9.py in TP1

--- TP1/ex9.py
+++ TP1/ex9.py
@@ -1,4 +1,6 @@
 def analyse_texte(texte):
-    return len(texte)
-a=str(input("enter a word :"))
+    word= texte.split()
+    word_len={word:len(word) for word in word}
+    return "number of characters:"+"" +str(len(texte)) +"\nnumber of words:"+""+ str(len(word))+"\n"+str(word_len)
+a=str(input("enter a phrase :"))
 print(analyse_texte(a))
